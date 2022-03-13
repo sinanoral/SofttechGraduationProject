d950640bbdf658e38b4707a27e424a833f141911
Sinan Oral <sinnanoral@gmail.com>
2022-03-13 03:52:40 +0300

    Add daos

---
+++ src/main/java/com/softtech/dao/CategoryDao.java
@@ -0,0 +1,9 @@
+package com.softtech.dao;
+
+import com.softtech.model.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryDao extends JpaRepository<Category, Long> {
+}
