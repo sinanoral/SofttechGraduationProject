528c72409ad9afa59561ec641157575609cc3d9e
Sinan Oral <sinnanoral@gmail.com>
2022-03-13 03:51:24 +0300

    Add results utility

---
+++ src/main/java/com/softtech/utility/results/ErrorResult.java
@@ -0,0 +1,12 @@
+package com.softtech.utility.results;
+
+public class ErrorResult extends Result {
+
+    public ErrorResult() {
+        super(false);
+    }
+
+    public ErrorResult(String message) {
+        super(false, message);
+    }
+}
