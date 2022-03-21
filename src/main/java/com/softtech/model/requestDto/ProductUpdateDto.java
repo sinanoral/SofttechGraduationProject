585af5c30c8921e93ee44b26f55beee71f5ca52a
Sinan Oral <sinnanoral@gmail.com>
2022-03-22 01:58:06 +0300

    Refactor constraints

--- src/main/java/com/softtech/model/requestDto/ProductUpdateDto.java
+++ src/main/java/com/softtech/model/requestDto/ProductUpdateDto.java
@@ -4,13 +4,13 @@ import lombok.Data;
 
 import javax.validation.constraints.DecimalMin;
 import javax.validation.constraints.Min;
-import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 @Data
 public class ProductUpdateDto {
-    @NotEmpty(message = "Name can not be empty!")
+    @NotBlank(message = "Name can not be blank!")
     @NotNull(message = "Name can not be null!")
     private String name;
 
--- src/main/java/com/softtech/model/requestDto/UserUpdateDto.java
+++ src/main/java/com/softtech/model/requestDto/UserUpdateDto.java
@@ -3,26 +3,26 @@ package com.softtech.model.requestDto;
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
 
-import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Data
 public class UserUpdateDto {
-    @NotEmpty(message = "User name can not be empty!")
+    @NotBlank(message = "User name can not be blank!")
     @NotNull(message = "User name can not be null!")
     @Length(min = 3, max = 20, message = "User name should be 3 to 20 character long!")
     private String userName;
 
-    @NotEmpty(message = "Password can not be empty!")
+    @NotBlank(message = "Password can not be blank!")
     @NotNull(message = "Password can not be null!")
     @Length(min = 6, max = 20, message = "Password should be 6 to 20 character long!")
     private String password;
 
-    @NotEmpty(message = "Name can not be empty!")
+    @NotBlank(message = "Name can not be blank!")
     @NotNull(message = "Name can not be null!")
     private String name;
 
-    @NotEmpty(message = "Surname can not be empty!")
+    @NotBlank(message = "Surname can not be blank!")
     @NotNull(message = "Surname can not be null!")
     private String surname;
 }
