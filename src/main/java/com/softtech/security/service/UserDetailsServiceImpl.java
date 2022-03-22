cc9eb2ff700ceaa0fe75e7289bcf34a1f66f9cb7
Sinan Oral <sinnanoral@gmail.com>
2022-03-23 01:44:56 +0300

    Update UserDetailsServiceImpl.java

--- src/main/java/com/softtech/security/service/UserDetailsServiceImpl.java
+++ src/main/java/com/softtech/security/service/UserDetailsServiceImpl.java
@@ -23,7 +23,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userService.findUserByUserName(username);
+        User user = userService.getUserByUserNameWithControl(username);
         return UserDetailsImpl.create(user);
     }
 
