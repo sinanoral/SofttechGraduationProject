80e91f01a08336805f34cc3c1badfb7339719859
Sinan Oral <sinnanoral@gmail.com>
2022-03-23 01:41:23 +0300

    Update CategoryMapper.java

--- src/main/java/com/softtech/mapper/CategoryMapper.java
+++ src/main/java/com/softtech/mapper/CategoryMapper.java
@@ -3,6 +3,7 @@ package com.softtech.mapper;
 import com.softtech.model.entity.Category;
 import com.softtech.model.responseDto.CategoryGetDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 import java.util.List;
@@ -10,5 +11,6 @@ import java.util.List;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface CategoryMapper {
-    List<CategoryGetDto> categoryListToCatergoryGetDtoList(List<Category> categories);
+    List<CategoryGetDto> categoryListToCategoryGetDtoList(List<Category> categories);
+    CategoryGetDto categoryToCategoryGetDto(Category category);
 }
