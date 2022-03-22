4348a2d28c0bdf34000dfea2a1d13416047dbac5
Sinan Oral <sinnanoral@gmail.com>
2022-03-23 01:40:22 +0300

    Change class name

--- src/main/java/com/softtech/model/responseDto/CategoryDetailDto.java
+++ src/main/java/com/softtech/model/responseDto/CategoryDetailDto.java
@@ -7,7 +7,7 @@ import java.math.BigDecimal;
 
 @Data
 @AllArgsConstructor
-public class CategoryDetailsDto {
+public class CategoryDetailDto {
     private String name;
     private BigDecimal vatRate;
     private BigDecimal minPrice;
