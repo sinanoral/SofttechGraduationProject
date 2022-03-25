d3185d31a101bef9c849dec2c52cd3f48249635a
Sinan Oral <sinnanoral@gmail.com>
2022-03-25 05:06:52 +0300

    Refacortor home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,93 +1,14 @@
-import {
-  Flex,
-  IconButton,
-  Table,
-  TableCaption,
-  Tbody,
-  Td,
-  Th,
-  Thead,
-  Tr,
-} from "@chakra-ui/react";
-import { DeleteIcon, EditIcon } from "@chakra-ui/icons";
-
+import { Flex } from "@chakra-ui/react";
 import CreateProduct from "../components/products/CreateProduct";
-import { api } from "../hooks";
-import { Product } from "../hooks/api/products/product";
+import ProductTable from "../components/products/ProductTable";
 
 export default function Home() {
-  const {
-    isLoading,
-    isError,
-    data: products,
-    error,
-    refetch,
-  } = api.useGetAllProducts();
-  const { mutate: deleteProductById } = api.useDeleteProductById();
-
-  if (isLoading) {
-    return <span>Loading...</span>;
-  }
-
-  if (isError) {
-    return <span>Error: {error.message}</span>;
-  }
-
   return (
     <Flex justify="space-between" w="100%">
       <Flex flexDir={"column"} w="40%">
         <CreateProduct />
       </Flex>
-      <Table variant="simple">
-        <TableCaption>Products</TableCaption>
-        <Thead>
-          <Tr>
-            <Th>Name</Th>
-            <Th>Category</Th>
-            <Th isNumeric>Price</Th>
-            <Th isNumeric>Vat amount</Th>
-            <Th isNumeric>Vat inclusive price</Th>
-            <Th isNumeric>Update</Th>
-            <Th isNumeric>Delete</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {products?.data.map((product: Product) => (
-            <Tr>
-              <Td>{product.name}</Td>
-              <Th>{product.categoryId}</Th>
-              <Th isNumeric>{product.price}</Th>
-              <Th isNumeric>{product.vatAmount}</Th>
-              <Th isNumeric>{product.vatInclusivePrice}</Th>
-              <Th>
-                <Flex justify="flex-end">
-                  <IconButton
-                    colorScheme="orange"
-                    aria-label="Update product"
-                    icon={<EditIcon />}
-                  />
-                </Flex>
-              </Th>
-              <Th>
-                <Flex justify="flex-end">
-                  <IconButton
-                    colorScheme="red"
-                    aria-label="Delete product"
-                    icon={<DeleteIcon />}
-                    onClick={() => {
-                      deleteProductById(product.id, {
-                        onSuccess: () => {
-                          refetch();
-                        },
-                      });
-                    }}
-                  />
-                </Flex>
-              </Th>
-            </Tr>
-          ))}
-        </Tbody>
-      </Table>
+      <ProductTable />
     </Flex>
   );
 }
